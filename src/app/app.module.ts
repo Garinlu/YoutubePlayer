d2dfbc10bd9a2b0756589023ea2156a199d23a1e
Lucas Garin <lucas.garin73@gmail.com>
2018-01-11 17:39:10 +0100

    add components : history, bookmarks and video ; Change from bootstrap to semantic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {SuiModule} from 'ng2-semantic-ui';
 
-
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { HistoryComponent } from './history/history.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { VideoComponent } from './video/video.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HistoryComponent,
+        BookmarksComponent,
+        VideoComponent
+    ],
+    imports: [
+        BrowserModule,
+        SuiModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
